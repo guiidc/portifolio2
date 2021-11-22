117e6a6a20904fdaad64ec63a1cca387f00251c2
Guilherme da Costa <guilhermedc93@gmail.com>
2021-11-22 09:38:32 -0300

    conclui header

---
+++ src/components/Header.jsx
@@ -0,0 +1,21 @@
+function Header () {
+  return (
+    <div className="header-wrapper">
+      <div className="header">
+        <div className="logo">
+          <h1>Guilherme da Costa</h1>
+          <span>Desenvolvedor web full-stack</span>
+          <div className="fill"></div>
+        </div>
+        <nav>
+          <a href="#1">Sobre</a>
+          <a href="#1">Projetos</a>
+          <a href="#1">Tecnologias</a>
+          <a href="#1">Comentários</a>
+          <a href="#1">Contato</a>
+        </nav>
+      </div>
+    </div>
+  )
+}
+export default Header;
